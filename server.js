1a99f894bd1dd35a7df07a84885869520f2ddc88
Tyler Agee <tyler@pyroturtle.com>
2016-10-05 11:59:02 -0700

    adding in support for post

--- server.js
+++ server.js
@@ -5,22 +5,40 @@ var request = require("request");
 var app = express();
 
 function callURL(url, callback) {
-    request(url, function (error, response, body) {
+
+    opt = {
+        method: url["method"],
+        uri: url["url"],
+    }
+
+    if (url["method"] === "POST") {
+        opt.json = true; // basically sets up stringify and other headers
+        opt.body = url["post"];
+    }
+
+    // console.log("opt", opt)
+
+    request(opt, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             // console.log("req body ", body)  // not needed debug statement
-            callback(null, JSON.parse(body));
+            try {
+                rtn_body = body;
+                callback(null, body);
+            } catch(e) {
+                callback(null, {"error": "unable to parse body", "opt": opt, "e": e, "body": body});
+            }  
         } else if (error) {
-            console.log(error);
+            console.log("error with request", error, "for", opt);
             msg = {"error": error, "url": url}
             callback(null, msg);
         } else {
-            callback(null, response.statusCode);
+            callback(null, {"error": "bad status code", "code": response.statusCode, "opt": opt});
         }
     })
 }
 
-
-app.use(bodyParser.json()); // for parsing application/json
+// app.use(bodyParser.urlencoded());
+app.use(bodyParser.json({limit: '100mb', extended: true})); // for parsing application/json
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -29,17 +47,22 @@ app.get('/', function (req, res) {
 app.post("/x", function (req, res){
     var start = new Date();
     // console.log(req.body);
-    console.log(req.body.urls);
+    // console.log(JSON.stringify(req.body));
+    // console.log(req.body.urls);
+    urls = req.body.urls
     var data = []
-    async.mapLimit(req.body.urls, 10, callURL, function(err, body) {
+    async.mapLimit(urls, 10, callURL, function(err, body) {
+        // console.log("got to after...")
         if (err) data.push(err);
         data.push(body)
         var end = new Date();
         data.push({"timings": {"total (ms)": end.getTime() - start.getTime()}})
+        // console.log(data);
         res.send(data);
     });
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    _json_log = {"level": "INFO", "msg": "Requester app listening on port 3000!"}
+    console.log(JSON.stringify(_json_log));
 });
